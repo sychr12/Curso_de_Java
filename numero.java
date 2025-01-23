319142c331864da72188cec7f18628c42c1473bc
Luiz Felipe <sayychr@gmail.com>
2025-01-23 11:37:39 -0300

    Add files via upload

---
+++ numero.java
@@ -0,0 +1,37 @@
+public class numero {
+    public static void main(String[] args) {
+        int x , y, a, c, n;
+        double b,B,h, area, m;
+        
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        x= 5;
+        y = 2 * x;
+
+        double resultado;
+        m = 5.0;
+        n = (int) m;
+
+        a= 5;
+        c = 2;
+
+        resultado = (double ) a/c;
+
+
+        area = (b+B) / 2.0 * h;
+
+        System.err.println(x);
+        System.err.println(y);
+
+        System.out.println(area);
+
+        System.out.println(n);
+
+        System.out.println(resultado);
+
+
+    }
+    
+}
