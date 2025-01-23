31bc8a8a614a1947d5170ba473f52245ab3337c3
Luiz Felipe <sayychr@gmail.com>
2025-01-23 11:56:52 -0300

    Add files via upload

--- teste.java
+++ teste.java
@@ -6,22 +6,22 @@ public class teste {
   public static void main(String[] args) {
 
     Locale.setDefault(Locale.US);
-    Scanner sc = new Scanner(System.in);
-
-    //pode trocar
-    String x;
-    int y;
-    double z;
-
-    x = sc.next();
-    y = sc.nextInt();
-    z = sc.nextDouble();
-    
-    System.out.printf("teste ");
-    System.out.println(x);
-    System.out.println(y);
-    System.out.println(z);
-
-    sc.close();
+      //pode trocar
+      try (Scanner sc = new Scanner(System.in)) {
+          //pode trocar
+          String x;
+          int y;
+          double z;
+          
+          System.out.println("Digite os dados");
+          x = sc.next();
+          y = sc.nextInt();
+          z = sc.nextDouble();
+          
+          System.out.printf("teste ");
+          System.out.println(x);
+          System.out.println(y);
+          System.out.println(z);
+      }
   }
 }
