46ca78087e8c7b416843022033aaf4e73b4119e0
Luiz Felipe <sayychr@gmail.com>
2025-01-24 08:15:42 -0400

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,17 @@
+public class java1 {
+    public static void main(String[] args) {
+        int a = 10,soma,soma1,soma3;
+        int b = 30;
+        int c = -30;
+        int d = 0;
+        soma = a + b;
+        soma1 = c + a;
+        soma3 = d + d;
+        System.out.println(soma);
+        System.out.println(soma1);
+        System.out.println(soma3);
+
+
+    }
+    
+}
